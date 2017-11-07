b554a582d07e9dac978b5999d4da6f4fa1e8a6f9
Jacob Hegna <jacobhegna@gmail.com>
2017-11-06 19:38:42 -0600

    Move TestSuite from main.cpp --> TestSuite.hpp

---
+++ TestSuite.hpp
@@ -0,0 +1,101 @@
+/**
+ * @author: jacob hegna 
+ * @date: 11/6/17
+ * @file TestSuite.hpp
+ * @brief Test class for LinkedListOfInts
+ */
+
+#include <iostream>
+#include "LinkedListOfInts.h"
+
+/**
+ * TestSuite
+ * @desc: test class which encapsulates all the tests for lab06
+ */
+class TestSuite {
+public:
+    TestSuite() {}
+    virtual ~TestSuite() {}
+
+    /**
+     * test_all
+     * @pre: nothing
+     * @post: nothing
+     * @return: true if every test returns true
+     * @desc: runs all the tests
+     */
+    bool test_all() {
+        bool success = true;
+
+        // testing function isEmpty
+        success &= t_isEmpty_empty();
+        success &= t_isEmpty_addBack();
+        success &= t_isEmpty_addFront();
+
+        return success;
+    } // end of test_all
+
+    /****
+     * Tests below
+     ***/
+
+    bool t_isEmpty_empty() {
+        LinkedListOfInts list;
+
+        auto list_vec = list.toVector();
+        return assert_test(list_vec.empty(), true, "isEmpty", "calls isEmpty on a fresh list");
+    }
+
+    bool t_isEmpty_addBack() {
+        LinkedListOfInts list;
+
+        for(int i = 0; i < 17; ++i) {
+            list.addBack(i);
+        }
+
+        auto list_vec = list.toVector();
+
+        for()
+
+        return assert_test(list_vec.empty(), false, "isEmpty", "pushes to list with addBack then calls isEmpty");
+    }
+
+    bool t_isEmpty_addFront() {
+        LinkedListOfInts list;
+
+        for(int i = 0; i < 17; ++i) {
+            list.addFront(i);
+        }
+
+        auto list_vec = list.toVector();
+        return assert_test(list_vec.empty(), false, "isEmpty", "pushes to list with addFront then calls isEmpty");
+    }
+
+    bool t_size_empty() {
+        LinkedListOfInts list;
+
+        auto list_vec = list.toVector();
+        return assert_test(list_vec.empty(), true, "isEmpty", "calls isEmpty on a fresh list");
+    }
+
+private: 
+
+    /**
+     * assert_test
+     * @pre: nothing
+     * @post: nothing
+     * @return: true if actual == expect
+     * @desc: assert, but doesn't halt execution or throw an exception if the assert fails
+     */
+    template<typename T>
+    bool assert_test(T actual, T expect, std::string test, std::string desc = "") {
+        if(expect == actual) {
+            std::cout << "[" << test << "]: Test \x1b[32mpassed\x1b[0m! Description: " << desc << std::endl;
+            return true;
+        } else {
+            std::cout << "[" << test << "]: Test \x1b[31mfailed\x1b[0m! Expected (" << expect << ") but given (" << actual << "). Description: " << desc << std::endl;
+            return false;
+        }
+    } // end of assert_test
+
+}; // end of class TestSuite
--- main.cpp
+++ main.cpp
@@ -4,73 +4,14 @@
 *	@file main.cpp
 *	@brief driver for LinkedList demo
 */
-#include <iostream>
-#include "LinkedListOfInts.h"
 
-#include "Test.hpp"
-
-template<typename T>
-bool assert_test(T actual, T expect, std::string test, std::string desc = "") {
-    if(expect == actual) {
-        std::cout << "[" << test << "]: Test \x1b[32mpassed\x1b[0m! Description: " << desc << std::endl;
-        return true;
-    } else {
-        std::cout << "[" << test << "]: Test \x1b[31mfailed\x1b[0m! Expected (" << expect << ") but given (" << actual << "). Description: " << desc << std::endl;
-        return false;
-    }
-}
-
-class TestSuite {
-public:
-    TestSuite() {}
-
-    bool test_all() {
-        bool success = true;
-
-        success &= t_isEmpty_empty();
-        success &= t_isEmpty_addBack();
-        success &= t_isEmpty_addFront();
-
-        return success;
-    }
-
-    bool t_isEmpty_empty() {
-        LinkedListOfInts list;
-
-        auto list_vec = list.toVector();
-        return assert_test(list_vec.empty(), true, "isEmpty", "calls isEmpty on a fresh list");
-    }
-
-    bool t_isEmpty_addBack() {
-        LinkedListOfInts list;
-
-        for(int i = 0; i < 17; ++i) {
-            list.addBack(i);
-        }
-
-        auto list_vec = list.toVector();
-        return assert_test(list_vec.empty(), false, "isEmpty", "pushes to list with addBack then calls isEmpty");
-    }
-
-    bool t_isEmpty_addFront() {
-        LinkedListOfInts list;
-
-        for(int i = 0; i < 17; ++i) {
-            list.addFront(i);
-        }
-
-        auto list_vec = list.toVector();
-        return assert_test(list_vec.empty(), false, "isEmpty", "pushes to list with addFront then calls isEmpty");
-    }
-
-};
-
+#include "TestSuite.hpp"
 
 int main(int argc, char** argv)
 {
     TestSuite tests;
     tests.test_all();
-    
+
     return 0;
 }
 
